93451ed53ed360f4217543131621d1dc12ca655b
HelmutDCI <helmut.ofori@dci-student.org>
2025-07-23 10:57:07 +0200

    update slye.css and script

--- calculaitor.js
+++ calculaitor.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //CALCULATOR program
 const display = document.getElementById("display");
 
@@ -17,24 +17,4 @@ function calculate(){
     catch(error){
         display.value = "error";
     }
-=======
-//CALCULATOR program
-const display = document.getElementById("display");
-
-function appendtoDisplay(input){
-    display.value += input;
-}
-
-function clearDisplay(){
-    display.value = "";
-}
-
-function calculate(){
-    try{
-        display.value = eval(display.value);
-    }
-    catch(error){
-        display.value = "error";
-    }
->>>>>>> 352c732a08afb54fa8fd5bf4157405e9c7b75bfc
 }
